addada41abdefe84a032e0a69992bae6cd8762f9
Michael M <trdMichaelM@yandex.ru>
2021-10-18 18:52:12 +0300

    ready for review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'reviews',
     'api',
     'django_filters',
