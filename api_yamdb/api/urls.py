c4d27fb52ede2a714a8b6eb190d9e106f47cca84
Slava <slavchick12@list.ru>
2021-10-18 17:52:35 +0300

    deleted djoser

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -29,7 +29,6 @@ router.register(
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
     path('v1/auth/signup/', signup, name='signup'),
     path('v1/auth/token/', token, name='token')
