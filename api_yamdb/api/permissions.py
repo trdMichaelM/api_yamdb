fb9fdc6cf3c3a805de8e69a4055fee3348846498
Давид <dovmont1rambler.ru@yandex.ru>
2021-10-21 14:12:12 +0300

    вариант f3

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -27,7 +27,7 @@ class AdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+                or request.user.is_authenticated)#test
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
