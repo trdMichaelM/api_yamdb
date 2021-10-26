b136f3a369b038ca54fead22115e040987079416
Давид <dovmont1rambler.ru@yandex.ru>
2021-10-26 09:05:23 +0300

    вариант q7

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,16 @@
 from rest_framework import permissions
 
+
 class IsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user.is_moderator
 
 class IsModeratorPermission(permissions.BasePermission):
     def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_moderator
+    def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_moderator
         
 class IsOwnerPermission(permissions.BasePermission):
@@ -16,8 +21,7 @@ class IsOwnerPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return (obj.author == request.user or request.user.is_admin
-                or  request.user.is_moderator)
+        return obj.author == request.user 
 
 class AdminReadOnlyPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -30,9 +34,9 @@ class AdminReadOnlyPermissions(permissions.BasePermission):
 class ReadOnlyOrAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         safe_method = request.method in permissions.SAFE_METHODS
-        return bool(safe_method or (request.user
-                                    and request.user.is_authenticated
-                                    and request.user.is_admin))
+        return bool(safe_method or request.user
+                    and request.user.is_authenticated
+                    and request.user.is_admin)
 
 
 class AdminWriteOnlyPermissions(permissions.BasePermission):
@@ -40,11 +44,17 @@ class AdminWriteOnlyPermissions(permissions.BasePermission):
         return bool(request.user and request.user.is_authenticated
                     and request.user.is_admin)
 
-                
 class ReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
+        
+class AdminOrReadOnly(permissions.BasePermission):
 
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
-
-    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user or request.user.is_admin or request.user.is_moderator
